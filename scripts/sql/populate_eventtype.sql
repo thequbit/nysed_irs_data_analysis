cbae65ab3ac7aba9481a2be0cc460b3d768d3146
thequbit <thequbit@gmail.com>
2013-03-27 23:21:40 -0400

    removed comma's from names and changed some caps

--- scripts/sql/populate_eventtype.sql
+++ scripts/sql/populate_eventtype.sql
@@ -20,8 +20,8 @@ INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Arson",
 INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Kidnapping",1,1);
 INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Kidnapping",0,1);
 
-INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Assault_with_Physical_Injury",1,1);
-INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Assault_with_Physical_Injury",0,1);
+INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Assault_With_Physical_Injury",1,1);
+INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Assault_With_Physical_Injury",0,1);
 
 INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Reckless_Endangerment",1,1);
 INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Reckless_Endangerment",0,1);
@@ -29,8 +29,8 @@ INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Reckles
 INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Minor_Altercations",1,1);
 INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Minor_Altercations",0,1);
 
-INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Intimidation,_Harassment,_Menacing,_or_Bullying",1,1);
-INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Intimidation,_Harassment,_Menacing,_or_Bullying",0,1);
+INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Intimidation_Harassment_Menacing_or_Bullying",1,1);
+INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Intimidation_Harassment_Menacing_or_Bullying",0,1);
 
 INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Burglary",1,1);
 INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Burglary",0,1);
@@ -38,8 +38,8 @@ INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Burglar
 INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Criminal_Mischief",1,1);
 INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Criminal_Mischief",0,1);
 
-INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Larceny,_or_Other_Theft",1,1);
-INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Larceny,_or_Other_Theft",0,1);
+INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Larceny_or_Other_Theft",1,1);
+INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Larceny_or_Other_Theft",0,1);
 
 INSERT INTO eventtypes(eventtypename, withweapon, weaponrelated) VALUES("Bomb_Threat_False_Alarm",0,0);
 
