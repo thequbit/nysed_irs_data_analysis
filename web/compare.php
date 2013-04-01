a817a548b983d9c36fa021683c46385181b0c9c7
thequbit <thequbit@gmail.com>
2013-04-01 05:25:08 -0400

    included forgotten css file, as well as added a link on the compare page

--- web/compare.php
+++ web/compare.php
@@ -2,7 +2,9 @@
 	require_once("_header.php");
 ?>
 
-	
+	Here is a langing page for where different comparisons can be made with database data.  Select from one of the below options to perform a comparison.
+
+	<a href="comparedistricts.php">Compare School Districts</a>
 
 <?php
 	require_once("_footer.php");
